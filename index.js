84ba636180e83ea369fd4d2bf7b81d71e452755a
Matdrox <Matdrox@users.noreply.github.com>
2023-01-28 15:40:33 +0100

    [Finished] Early Prototype

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
+require('dotenv').config()
+
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const configuration = new Configuration({
+  organization: 'org-tuOiA1rN0intH8Us6sOieFSR',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-})
+app.post('/', async (req, res) => {
+
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Translate the following to Interlingua: "${message}"`,
+    max_tokens: 10,
+    temperature: 0,
+  });
+  console.log(response.data);
+
+  if (response.data.choices[0].text) {
+    res.json({ message: response.data.choices[0].text });
+  }
+});
 
 app.listen(port, () => {
-	console.log('Example app listening')
+  console.log('Example app listening');
 });
--- src/App.js
+++ src/App.js
@@ -1,1 +1,37 @@
 import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3001/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => setResponse(data.message));
+  };
+
+  return (
+    <div className='App'>
+      <h1>English to Interlingua Translator</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <br/>
+        <button type='submit'>Translate</button>
+      </form>
+      <div>{response}</div>
+    </div>
+  );
+}
+
+export default App
