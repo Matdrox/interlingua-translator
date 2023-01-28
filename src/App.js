6829eb7a81a32d6acc83aa2989d2f00fc2c9e576
Matdrox <Matdrox@users.noreply.github.com>
2023-01-28 16:10:53 +0100

    [Implemented] TailwindCSS

--- src/App.js
+++ src/App.js
@@ -19,19 +19,37 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <h1>English to Interlingua Translator</h1>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        <br/>
-        <button type='submit'>Translate</button>
-      </form>
-      <div>{response}</div>
+    <div className='flex justify-center px-14 py-5'>
+      <div className='flex-col justify-center w-screen'>
+        <h1 className='text-3xl text-center'>
+          English to Interlingua Translator
+        </h1>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <div className='flex w-full'>
+            <textarea
+              className='w-1/2 h-60 text-xl p-2 resize-none border-2 bg-orange-50 border-slate-600 rounded-md'
+              value={message}
+              placeholder='Write in English'
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            <div className='w-1/2 h-60 text-xl p-2 bg-purple-50 border-2 border-slate-600 rounded-md'>
+              {response}
+            </div>
+          </div>
+          <br/>
+          <div className='w-full text-xl flex justify-center'>
+            <button
+              className='bg-purple-500 p-4 text-white rounded-lg'
+              type='submit'
+            >
+              Translate
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
